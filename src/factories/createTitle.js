f0e01b8afaed8abff77dfe311478a10f1fb7994b
Ernest Okot <okot08@gmail.com>
2017-06-08 17:49:45 +0300

    refactored out title creation

---
+++ src/factories/createTitle.js
@@ -0,0 +1,10 @@
+import Plottable from 'plottable';
+
+
+export const createTitle = ({title = null, subtitle = null, titleAlignment = 'left'}) => {
+  if (!title) return null;
+
+  return new Plottable.Components.TitleLabel(title, 0)
+    .xAlignment(titleAlignment)
+    .yAlignment('top');
+};
