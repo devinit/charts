2daff8fea8b5d6aa13c1aaffaa314585723cb12a
Ernest Okot <okot08@gmail.com>
2017-06-13 14:14:17 +0300

    added default values to schema publisher

--- schema/publish.js
+++ schema/publish.js
@@ -14,8 +14,13 @@ exports.publish = function (data) {
       return Object.assign(all, {
         [name]: {
           access,
+          name,
           type: type.names.join(),
-          properties: properties.map(({name, type: {names}, description}) => {
+          properties: properties.map(property => {
+
+            const {name, type: {names}, description, defaultvalue} = property;
+
+            console.log(defaultvalue);
 
             const literals = names.filter(n => n.match(/'.+'/)).map(n => n.match(/'(.+)'/)[1]);
             const nonLiterals = names.filter(n => !n.match(/'.+'/));
@@ -25,6 +30,7 @@ exports.publish = function (data) {
               type: literals.length ? undefined : nonLiterals[0],
               options: literals.length > 1 ? literals : undefined,
               value: literals.length === 1 ? literals[0] : undefined,
+              default: defaultvalue,
               description
             }
 
@@ -35,7 +41,7 @@ exports.publish = function (data) {
 
   const normalised = Object.keys(definitions)
     .filter(k => definitions[k].access === 'public')
-    .map(k => Object.assign(definitions[k], {name: k}))
+    .map(k => definitions[k])
     .map(({name, description, type, properties}) => {
       const parent = definitions[type];
       let allProperties = properties;
@@ -45,26 +51,24 @@ exports.publish = function (data) {
         allProperties = allProperties.concat(parent.properties.filter(p => !p.name.match(matchAlreadySet)))
       }
 
-      allProperties = allProperties.map(({name, type, value, options, description}) => {
-        const parent = definitions[type];
-        if (parent) {
-          return {
-            name,
-            value,
-            options,
-            key: `config.${name}`,
-            properties: parent.properties.map(p => Object.assign(p, {key: `config.${name}.${p.name}`})),
-            description
-          }
-        }
-
-        return {name, key: `config.${name}`, type, description, value, options}
-      });
-
       return {
         name,
         description,
-        properties: allProperties
+        properties: allProperties.map(({name, type, value, options, description, defaultValue}) => {
+          const parent = definitions[type];
+
+          if (parent) {
+            return {
+              name,
+              key: `config.${name}`,
+              properties: parent.properties.map(p => Object.assign(p, {key: `config.${name}.${p.name}`})),
+              description,
+              defaultValue
+            }
+          }
+
+          return {name, key: `config.${name}`, type, description, value, options, defaultValue}
+        })
       };
     });
 
