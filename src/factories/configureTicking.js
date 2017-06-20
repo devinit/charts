05a895ea4ef069caa91f6076a427e9a34eb555cb
Ernest Okot <okot08@gmail.com>
2017-06-20 07:15:45 +0300

    adds time axis ticking, removes a bunch or dead code

--- src/factories/configureTicking.js
+++ src/factories/configureTicking.js
@@ -1,37 +1,49 @@
+import Plottable from 'plottable';
+
+export const configureTimeAxisTicking = (axis, ticking = 'years', tickingStep = 1) => {
+
+  const tiers = [];
+
+  if (ticking === 'years') {
+    tiers.push({
+      formatter: new Plottable.Formatters.time("%Y"),
+      interval: Plottable.TimeInterval.year,
+      step: tickingStep,
+    })
+  }
+
+  if (ticking === 'months') {
+    tiers.push({
+      formatter: new Plottable.Formatters.time("%B"),
+      interval: Plottable.TimeInterval.month,
+      step: tickingStep
+    })
+  }
+
+  if (ticking === 'days') {
+    tiers.push({
+      formatter: new Plottable.Formatters.time("%d"),
+      interval: Plottable.TimeInterval.day,
+      step: tickingStep
+    })
+  }
+
+  if (ticking === 'hours') {
+    tiers.push({
+      formatter: new Plottable.Formatters.time("%H:%M"),
+      interval: Plottable.TimeInterval.hour,
+      step: tickingStep
+    })
+  }
+
+  axis.axisConfigurations([tiers])
+};
+
 export const configureAxisTicking = (axis, ticking) => {
-
-  if (ticking === 'all') {
-    axis.addClass('axis-tick-all')
-  }
-
-  if (ticking === 'even') {
-    axis.addClass('axis-tick-even')
-  }
-
-  if (ticking === 'odd') {
-    axis.addClass('axis-tick-odd')
-  }
-
-  if (ticking === 'sparse') {
-    axis.tickLabelMaxLines && axis.tickLabelMaxLines(1);
-    axis.addClass('axis-tick-sparse')
-  }
+  axis.addClass(`axis-tick-${ticking}`);
+  axis.tickLabelMaxLines && axis.tickLabelMaxLines(1);
 };
 
 export const configureGridTicking = (grid, ticking) => {
-  if (ticking === 'all') {
-    grid.addClass('gridlines-tick-all')
-  }
-
-  if (ticking === 'even') {
-    grid.addClass('gridlines-tick-even')
-  }
-
-  if (ticking === 'odd') {
-    grid.addClass('gridlines-tick-odd')
-  }
-
-  if (ticking === 'sparse') {
-    grid.addClass('gridlines-tick-sparse')
-  }
+  grid.addClass(`gridlines-tick-${ticking}`)
 };
