c8965cac591ad88537d878319c352a242eebd7d0
Ernest Okot <okot08@gmail.com>
2017-06-27 19:27:45 +0300

    added support for color palette and update examples

--- src/index.js
+++ src/index.js
@@ -1,11 +1,35 @@
-export const draw = ({element, data, config}) => {
+export const palette = new Map();
+
+export const setColorPalette = (colors) => {
+  colors.forEach(({name, color}) => palette.set(name, color));
+};
+
+export const draw = ({element, data, config: {colors, ...config}}) => {
 
   return new Promise(function (resolve, reject) {
     if (!config.type) return reject(new Error('No chart type specified'));
 
     import(`./charts/${config.type}.js`)
       .then(function (chart) {
-        resolve(chart.default(element, data, config));
+        resolve(chart.default(
+          element,
+          // Substitute color indicators
+          config.coloring ?
+            data.map(d => ({
+              ...d,
+              [config.coloring]: palette.get(d[config.coloring]) || d[config.coloring]
+            })) :
+            data,
+
+          {
+            ...config,
+
+            // Substitute colors[]
+            colors: palette.size && colors ?
+              colors.map(color => palette.get(color) || color) :
+              colors,
+          }
+        ));
       })
       .catch(error => {
         reject(error)
