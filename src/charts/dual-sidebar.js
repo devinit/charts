928e24fde82e32a0b90d9bf6934c20fc5e32a509
Ernest Okot <okot08@gmail.com>
2017-07-31 12:41:39 +0300

    added sort to dual sibebar groups and subgroups

--- src/charts/dual-sidebar.js
+++ src/charts/dual-sidebar.js
@@ -142,8 +142,8 @@ export default (element, data, config) => {
 
       const plots = [leftPlot, rightPlot];
 
-      const splittingIds = makeUnique(data.map(d => d[splitBy]));
-      const groupIds = makeUnique(data.map(d => d[groupBy]));
+      const splittingIds = makeUnique(data.map(d => d[splitBy])).sort((a, b) => a > b ? 1 : -1);
+      const groupIds = makeUnique(data.map(d => d[groupBy])).sort((a, b) => a > b ? 1 : -1);
       const subGroupIds = groupIds.map(groupId => makeUnique(
         data
           .filter(d => d[groupBy] === groupId)
