8393e5802096c43b16eb3392296e9c5909ac216f
Ernest Okot <okot08@gmail.com>
2017-08-09 02:13:58 +0300

    added scatter annotations API

--- src/factories/createScatterChart.js
+++ src/factories/createScatterChart.js
@@ -5,6 +5,7 @@ import {createScatterGridLines} from "./createGrid";
 import {createChartTable} from "./createTable";
 import {createTitle} from "./createTitle";
 import {createColorLegend} from "./createLegend";
+import {color} from "d3";
 
 /**
  * @typedef {Object} ScatterChart - Scatter chart configuration
@@ -54,6 +55,8 @@ export default ({element, plot, config}) => {
     bubble,
 
     legend = {},
+
+    annotations = [],
 
   } = config;
 
@@ -134,7 +137,15 @@ export default ({element, plot, config}) => {
       .range(Object.keys(mapping).map((_, index) => colors[index] || '#abc'));
 
     scatterPlot.datasets(datasets.map(d => new Plottable.Dataset(d)));
+
+    createScatterAnnotations({
+      annotations,
+      verticalScale,
+      horizontalScale,
+      plot
+    })
   };
+
   return {
 
     table,
@@ -148,6 +159,68 @@ export default ({element, plot, config}) => {
     }
   }
 }
+
+const createScatterAnnotations = ({annotations, verticalScale, horizontalScale, plot}) => {
+
+  plot.background().selectAll('*').remove();
+
+  plot.foreground().selectAll('*').remove();
+
+  annotations.forEach(({title, body, fill, horizontalAxis = {}, verticalAxis = {}}) => {
+    const x0 = horizontalScale.scale(
+      typeof horizontalAxis.minimum !== 'number' || horizontalAxis.minimum < horizontalScale.domainMin() ?
+        horizontalScale.domainMin() :
+        horizontalAxis.minimum
+    );
+
+    const x1 = horizontalScale.scale(
+      !horizontalAxis.maximum || horizontalAxis.maximum > horizontalScale.domainMax() ?
+        horizontalScale.domainMax() :
+        horizontalAxis.maximum
+    );
+
+    const y0 = verticalScale.scale(
+      !verticalAxis.maximum || verticalAxis.maximum > verticalScale.domainMax() ?
+        verticalScale.domainMax() :
+        verticalAxis.maximum
+    );
+
+    const y1 = verticalScale.scale(
+      !verticalAxis.minimum || verticalAxis.minimum < verticalScale.domainMin() ?
+        verticalScale.domainMin() :
+        verticalAxis.minimum
+    );
+
+    const colorFill = fill || '#ccc';
+    const {r, g, b} = color(colorFill).rgb();
+    const brightness = (r + g + b) / (256 * 3);
+
+    plot
+      .background()
+      .append('rect')
+      .attr('x', x0)
+      .attr('y', y0)
+      .attr('width', x1 - x0)
+      .attr('height', y1 - y0)
+      .attr('fill', fill || '#ccc')
+      .attr('opacity', '0.5');
+
+    plot
+      .foreground()
+      .append('foreignObject')
+      .attr('width', x1 - x0)
+      .attr('height', y1 - y0)
+      .attr('x', x0)
+      .attr('y', y0)
+      .html(`
+            <div class="${brightness > 0.5 ? 'dark' : 'light'}-label plot-label" style="text-align: left">
+              <div class="plot-label-header">${title}</div>
+              <div class="plot-label-value">${body || ''}</div>
+            </div>
+          `);
+
+  })
+};
 
 const createScatterPlot = ({plot, horizontalScale, verticalScale, bubbleScale}) => {
   return plot
