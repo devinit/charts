3e579ee0897e20039d9ad3c318d36f5257f84298
epicallan <epicallan.al@gmail.com>
2018-01-15 15:02:02 +0300

    treemap label improvements

--- src/factories/autofit/text.js
+++ src/factories/autofit/text.js
@@ -6,19 +6,19 @@ export const autoFitMetrics = (width, height, text) => {
   const estimatedArea = longestWordLength * (baseFont * 0.6) * words.length * baseFont;
 
   const areaRatio = Math.floor(availableArea / estimatedArea);
-  const widthRatio = width / (longestWordLength * (baseFont * 0.6) * words.length);
+  const widthRatio = width / (longestWordLength * (baseFont * 0.6));
 
   if (areaRatio < 2) return {fontSize: 0, showLabels: false};
 
   if (areaRatio < 4) {
-    return {fontSize: baseFont * 1.2, showLabels: widthRatio > 0.7};
+    return {fontSize: baseFont * 1.2, showLabels: widthRatio > 1};
   }
 
   if (areaRatio < 6) {
-    return {fontSize: baseFont * 1.5, showLabels: widthRatio > 0.7};
+    return {fontSize: baseFont * 1.5, showLabels: widthRatio > 1};
   }
 
-  return {fontSize: baseFont * 1.8, showLabels: widthRatio > 0.7};
+  return {fontSize: baseFont * 1.8, showLabels: widthRatio > 1};
 };
 
 export const autofitStyles = (width, height, text) => {
