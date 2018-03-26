8c9a59dc0dd8446d1a44e9f9c6bb809cdc1561a8
epicallan <epicallan.al@gmail.com>
2018-03-26 11:37:10 +0300

    webpack config setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,88 +1,87 @@
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HappyPack = require('happypack');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
 
 const isProductionBuild = process.env.WEBPACK_ENV === 'production';
 const jsOutputFilename = isProductionBuild ? 'di-charts.min.js' : 'di-charts.js';
 const cssOutputFilename = isProductionBuild ? 'di-charts.min.css' : 'di-charts.css';
 
-const extractLess = new ExtractTextPlugin(cssOutputFilename);
+// const limitChunkCountPlugin = new webpack.optimize.LimitChunkCountPlugin({
+//   maxChunks: 1
+// });
 
-const limitChunkCountPlugin = new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 });
+// const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+// const uglifyJsPlugin = new UglifyJSPlugin({
+//   beautify: false,
+//   mangle: {
+//     screw_ie8: true,
+//     keep_fnames: true,
+//   },
+//   compress: {
+//     screw_ie8: true,
+//     warnings: true
+//   },
+//   comments: false,
+// });
 
-const uglifyJsPlugin = new UglifyJSPlugin({
-  beautify: false,
-  mangle: {
-    screw_ie8: true,
-    keep_fnames: true,
-  },
-  compress: {
-    screw_ie8: true,
-  },
-  comments: false,
-});
 
 module.exports = [
   {
-    entry: [`${__dirname}/src.less`],
+    entry: [`${__dirname}/src/index.less`],
 
     output: {
       path: `${__dirname}/dist/`,
       filename: cssOutputFilename,
     },
-
     module: {
-      loaders: [
-        {
-          test: /\.less$/,
-          use: extractLess.extract([
-            { loader: 'css-loader', options: { minimize: isProductionBuild } },
-            { loader: 'less-loader' },
-          ]),
-        },
-      ],
+      rules: [{
+        test: /\.less$/,
+        use: ['css-loader', 'less-loader']
+      }]
     },
+    plugins: [
+      new ProgressBarPlugin(),
+    ]
 
-    plugins: [extractLess],
-
-    devtool: 'source-map',
   },
-
   {
-    entry: [`${__dirname}/src.js`],
-
+    entry: [`${__dirname}/src/index.ts`],
     output: {
       path: `${__dirname}/dist/`,
-      filename: `${jsOutputFilename}`,
-      chunkFilename: `[id]-${jsOutputFilename}`,
+      filename: '[name].js',
       library: 'DiCharts',
       libraryTarget: 'window',
     },
-
     module: {
-      loaders: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              cacheDirectory: true,
-              presets: [['env', { modules: false }], 'flow'],
-            },
-          }
-        },
-
-        {
-          test: /\.less$/,
-          use: extractLess.extract(['css-loader', 'less-loader']),
-        },
-      ],
+      rules: [{
+        test: /\.(ts|tsx)(\?[^?]*)?$/,
+        loader: 'happypack/loader?id=ts'
+      }],
     },
 
-    plugins: [limitChunkCountPlugin, extractLess, ...(isProductionBuild ? [uglifyJsPlugin] : [])],
-
-    devtool: 'source-map',
-  },
+    plugins: [
+      new HappyPack({
+        threads: 4,
+        id: 'ts',
+        loaders: [{
+          loader: 'ts-loader',
+          query: {
+            happyPackMode: true
+          },
+          options: {
+            transpileOnly: true
+          }
+        }]
+      }),
+      new ForkTsCheckerWebpackPlugin({
+        checkSyntacticErrors: true
+      }),
+      // limitChunkCountPlugin,
+      new ProgressBarPlugin(),
+    ],
+    devtool: isProductionBuild ? 'source-map' : 'inline-source-map'
+  }
 ];
