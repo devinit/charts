ab611eaaa8061cb4426fb88f1d0f3b99be81719d
epicallan <epicallan.al@gmail.com>
2018-04-06 18:25:05 +0300

    bar charts wip

---
+++ packages/bar/src/categoric/index.ts
@@ -0,0 +1,138 @@
+import * as Plottable from 'plottable';
+import { color } from 'd3';
+import {approximate} from '@devinit/prelude/lib/numbers';
+import { createCategoricChart, CategoricChart, CategoricConfig, BarPlot } from '@devinit-charts/core/lib/categoric';
+import {Labeling} from '@devinit-charts/core/lib/types';
+import createBarTipper from '../tooltip';
+import {createCustomLabels} from '../labeler';
+import { Component } from 'plottable';
+import { BarOrientation } from 'plottable/build/src/plots';
+
+// export type BarPlot = Plots.Bar<any, any>;
+export type Config = CategoricConfig & {
+  interactions?: {enable?: boolean};
+  highlight?: string[]; // bars to highlight
+  tooltips?: {enable?: boolean};
+};
+
+export interface BarInteractionOpts {
+  orientation?: BarOrientation;
+  labeling?: Labeling;
+  highlight?: string[];
+}
+
+const createBarInteraction = (opts: BarInteractionOpts) =>  {
+  const {orientation = 'vertical', labeling, highlight = []} = opts;
+
+  const { prefix = '', suffix = '' } = labeling || {};
+
+  let currentId: string | null = null;
+
+  const reset = plot => {
+    plot.entities().forEach(entity => {
+      if (!entity.selection.attr('initial-fill')) {
+        entity.selection.attr('initial-fill', entity.selection.attr('fill'));
+      } else {
+        entity.selection.attr('fill', entity.selection.attr('initial-fill'));
+      }
+    });
+
+    plot
+      .foreground()
+      .select('text')
+      .remove();
+  };
+
+  return (plot: BarPlot) => {
+    // QUICKFIX: Highlight some bars in a bar chart
+    if (highlight.length) {
+      plot
+        .entities()
+        .filter((entity => highlight.indexOf(entity.datum.label) > -1))
+        .forEach(entity => {
+          const node = entity.selection.node() as any; // TODO: fix me, this is a hack
+          if (!node) return plot;
+          const {
+            x, y, width, height
+          } = node.getBBox();
+
+          plot
+            .background()
+            .append('text')
+            .attr('class', 'plot-label hover-label')
+            .attr('x', orientation === 'vertical' ? (x + width) / 2 : width + x + 5)
+            .attr('y', orientation === 'horizontal' ? (y + height) / 2 : y - 10)
+            .attr('text-anchor', orientation === 'vertical' ? 'middle' : 'start')
+            .attr('style', `fill: ${entity.selection.attr('fill')}`)
+            .text(`${prefix}${approximate(entity.datum.value)}${suffix}`);
+        });
+    }
+
+    const interaction = new Plottable.Interactions.Pointer()
+      .onPointerExit(() => {
+        reset(plot);
+        currentId = null;
+      })
+      .onPointerMove(point => {
+        const [entity] = plot.entitiesAt(point);
+        const id = entity && `${entity.position.x.toFixed(3)}-${entity.position.y.toFixed(3)}`;
+
+        if (id && id !== currentId) {
+          reset(plot);
+          const node = entity.selection.node() as any;
+          const {
+            x, y, width, height
+          } = node.getBBox();
+
+          plot
+            .foreground()
+            .append('text')
+            .attr('class', 'plot-label hover-label')
+            .attr('x', orientation === 'vertical' ? x + width / 2 : width + x + 5)
+            .attr('y', orientation === 'horizontal' ? y + height / 2 : y - 10)
+            .attr('text-anchor', orientation === 'vertical' ? 'middle' : 'start')
+            .attr('style', `fill: ${entity.selection.attr('fill')}`)
+            .text(`${prefix}${approximate(entity.datum.value)}${suffix}`);
+
+          const fill = color(entity.selection.attr('initial-fill')).darker(0.7);
+
+          entity.selection.attr('fill', fill.toString());
+
+          currentId = id;
+        }
+      });
+
+    interaction.attachTo(plot);
+
+    plot.onDetach(_plot => {
+      interaction.detachFrom(_plot);
+    });
+  };
+};
+
+export default (element: HTMLElement, plot: BarPlot, config: Config): CategoricChart => {
+  const {
+    interactions = { enable: false },
+    tooltips = { enable: true },
+    labeling,
+    orientation,
+    highlight
+  } = config;
+  const chart: CategoricChart = createCategoricChart({ element, plot, config });
+
+  plot.onAnchor((_plotComponent: Component) => {
+    setTimeout(() => {
+      if (interactions.enable) {
+        createBarInteraction({orientation, labeling, highlight})(plot);
+      }
+      if (config.labeling.custom) {
+        createCustomLabels({prefix: labeling.prefix, plot});
+      }
+      if (tooltips.enable) {
+        createBarTipper(element, config.labeling, config.orientation || 'vertical')(plot);
+      }
+    }, 500);
+  });
+
+  return chart;
+};
---
+++ packages/bar/src/tooltip/index.ts
@@ -0,0 +1,115 @@
+import * as Plottable from 'plottable';
+import Tooltip from 'tooltip.js';
+import {Labeling} from '@devinit-charts/core/lib/types';
+import { approximate } from '@devinit/prelude/lib/numbers';
+import { BarPlot } from '@devinit-charts/core/lib/categoric';
+
+export default (container: HTMLElement, labeling: Labeling, orientation: string) => {
+  let currentHash = null;
+  const {
+    prefix = '',
+    suffix = ''
+  } = labeling;
+  return (plot: BarPlot) => {
+    const tooltipAnchor = plot
+      .foreground()
+      .append('circle')
+      .attr('r', 3)
+      .attr('fill', 'transparent');
+
+    const placement = _orientation => {
+      return {
+        'vertical': 'top',
+        'horizontal': 'right',
+        'inverted-vertical': 'bottom',
+        'inverted-horizontal': 'left',
+      }[_orientation];
+    };
+
+    const tip = Tooltip(tooltipAnchor.node(), {
+      title: 'Tooltip',
+      placement: placement(orientation),
+      container,
+      template: `
+          <div class="tooltip" role="tooltip">
+            <div class="tooltip-arrow"></div>
+            <div id="tt-title" class="tooltip-inner"></div>
+            <div id="tt-body" class="tooltip-body"></div>
+          </div>
+      `,
+    });
+
+    const template = datum => {
+      const value = /inverted/.test(orientation) ? Math.abs(datum.value) : datum.value;
+      return `
+          <div>
+            <span style="background: ${datum.color}; width: 10px; height: 10px; display: inline-block"></span>
+            <span>${datum.group ? `${datum.group}:` : ''} ${prefix}${approximate(value)}${suffix}</span>
+          </div>
+        `;
+    };
+
+    const getEntities = point =>
+      plot.entitiesIn(
+        orientation === 'vertical'
+          ? {min: point.x - 1, max: point.x + 1}
+          : {min: 0, max: plot.width()},
+        orientation !== 'vertical'
+          ? {min: point.y - 1, max: point.y + 1}
+          : {min: 0, max: plot.height()},
+      );
+
+    const getPosition = entities =>
+      entities
+        .reduce(
+          ([p], e) => [
+            {
+              x: [...p.x, e.position.x],
+              y: [...p.y, e.position.y],
+            },
+          ],
+          [{x: [], y: []}],
+        )
+        .map(({x, y}) => ({
+          x: orientation === 'vertical' ? x.reduce((sum, k) => sum + k, 0) / x.length : Math.min.apply(null, x),
+          y: orientation !== 'vertical' ? y.reduce((sum, k) => sum + k, 0) / y.length : Math.min.apply(null, y),
+        }))
+        .reduce((_, d) => d);
+
+    const interaction = new Plottable.Interactions.Pointer()
+      .onPointerExit(() => {
+        tip.hide();
+        currentHash = null;
+      })
+      .onPointerMove(point => {
+        const entities = getEntities(point);
+
+        const position = getPosition(entities);
+
+        const sum = entities.reduce((_sum, e) => _sum + e.datum.value, 0);
+
+        const hash = position.x.toString() + position.y.toString();
+
+        if (entities.length && sum && currentHash !== hash) {
+          requestAnimationFrame(() => {
+            tip.hide();
+            tooltipAnchor.attr('cx', position.x);
+            tooltipAnchor.attr('cy', position.y);
+            tip.show();
+            tip._tooltipNode.querySelector('#tt-title').innerText = entities[0].datum.label;
+            tip._tooltipNode.querySelector('#tt-body').innerHTML = entities
+              .map(e => template(e.datum))
+              .join('');
+            currentHash = hash;
+          });
+        }
+      });
+
+    interaction.attachTo(plot);
+
+    plot.onDetach(_plot => {
+      interaction.detachFrom(_plot);
+      tip.dispose();
+    });
+  };
+};
