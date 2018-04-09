343fa34bf194cf5a8ace1d1d0d7c2e8bab054d41
epicallan <epicallan.al@gmail.com>
2018-04-09 14:14:07 +0300

    get storybook to work

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,38 +1,17 @@
-const path = require('path');
-const webpack = require('webpack');
-const HappyPack = require('happypack');
-const packageJSON = require('../package.json');
-const ProgressBarPlugin = require('progress-bar-webpack-plugin');
-const TSDocgenPlugin = require("react-docgen-typescript-webpack-plugin");
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
-const SRC_PATH = path.join(__dirname, '../packages');
-
-const configFile = path.join(__dirname, './tsconfig.json');
+const TSDocgenPlugin = require('react-docgen-typescript-webpack-plugin');
 
 const newRules = [
   {
       test: /\.(ts|tsx)(\?[^?]*)?$/,
-      loader: 'awesome-typescript-loader',
-      options: {configFile}
+      loader: 'awesome-typescript-loader'
   }
 ]
 
 const newExtensions =  ['.ts', '.tsx', '.js'];
 
 const newPlugins = [
-  new TSDocgenPlugin(),
-  // new ProgressBarPlugin(),
-  // new HappyPack({
-  //   threads: 4,
-  //   id: 'ts',
-  //   loaders: [{
-  //     loader: 'ts-loader',
-  //     query: { happyPackMode: true },
-  //     options: { transpileOnly: true, configFile }
-  //   }]
-  // }),
-  // new ForkTsCheckerWebpackPlugin({ checkSyntacticErrors: true }),
+  new TSDocgenPlugin()
 ];
 
 
@@ -44,7 +23,7 @@ module.exports = (baseConfig, env, config) => {
   //   loader: require.resolve("awesome-typescript-loader")
   // });
   config.module.rules.push(...newRules);
-  // config.plugins.push(...newPlugins);
+  config.plugins.push(...newPlugins);
   config.resolve.extensions.push(...newExtensions);
   return config;
 };
--- packages/bubble/src/stories/index.tsx
+++ packages/bubble/src/stories/index.tsx
@@ -7,6 +7,13 @@ const data: BubbleData[] = [
     { x: 1, y: 1 }, { x: 2, y: 3 }, { x: 3, y: 2 },
     { x: 4, y: 4 }, { x: 5, y: 3 }, { x: 6, y: 5 }
 ];
+const Frame = ({children}) =>
+    <div style={{border: 'solid 2px black', width: '500px', height: '500px'}}>
+        {...children}
+    </div>;
 
 storiesOf('BubbleChart', module)
-    .add('simple', () => <BubbleChart data={data} />);
+    .add('simple', () =>
+        <Frame>
+            <BubbleChart data={data} />
+        </Frame>);
